dfbca2f51ba5150836cd6c18b1c86fa0bee1aabc
Ricci Adams <git@ricciadams.com>
2012-01-11 19:42:31 -0800

    Bumping copyright to 2012.

--- Source/SwiffHTMLToCoreTextConverter.m
+++ Source/SwiffHTMLToCoreTextConverter.m
@@ -1,6 +1,6 @@
 /*
     SwiffHTMLToCoreTextConverter.m
-    Copyright (c) 2011, musictheory.net, LLC.  All rights reserved.
+    Copyright (c) 2011-2012, musictheory.net, LLC.  All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
     modification, are permitted provided that the following conditions are met:
